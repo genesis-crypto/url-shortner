0a1f3ba3040841cf337759ae771c5ce4727387f1
Pedro <p-cardozo@hotmail.com>
2023-02-22 20:32:28 -0300

    feat(users): adding factories for controllers

--- src/domains/users/controllers/index.ts
+++ src/domains/users/controllers/index.ts
@@ -1,5 +1,8 @@
 import { GetOneUser } from "./get-one-user"
 import { GetManyUser } from "./get-many-user"
+import { PostUser } from "./post-user"
+import { DeleteUser } from "./delete-user"
+import { PatchUser } from "./patch-user"
 
 function makeGetOneUser() {
     return new GetOneUser()
@@ -9,4 +12,16 @@ function makeGetManyUser() {
     return new GetManyUser()
 }
 
-export { makeGetOneUser, makeGetManyUser }
+function makePostUser() {
+    return new PostUser()
+}
+
+function makePatchUser() {
+    return new PatchUser()
+}
+
+function makeDeleteUser() {
+    return new DeleteUser()
+}
+
+export { makeGetOneUser, makeGetManyUser, makePostUser, makePatchUser, makeDeleteUser }
