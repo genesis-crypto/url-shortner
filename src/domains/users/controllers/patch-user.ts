e4cc79f7f7d81d7fb80ed35e8bd6ddedd99481bb
Pedro <p-cardozo@hotmail.com>
2023-02-22 20:56:41 -0300

    feat(users): finishing PatchUser controller

--- src/domains/users/controllers/patch-user.ts
+++ src/domains/users/controllers/patch-user.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express'
 import { Controller } from '../../../shared/interfaces/controller.interface'
+import { UserModel } from '../models/user'
 
 export class PatchUser implements Controller {
     async handle(req: Request, res: Response) {
-        return res.status(200).send('todo')
+        const {params, body} = req
+
+        if (!body.name || !params.uuid) {
+            return res.status(400).send('missing content or params')
+        }
+
+        const result = await new UserModel().updateOneUser(params.uuid, body)
+
+        return res.status(200).send(result)
     }
 }
