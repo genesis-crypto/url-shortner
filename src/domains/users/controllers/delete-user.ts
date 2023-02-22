df6c0c9c8478bd99ce301fbe0195d27bd03c50f7
Pedro <p-cardozo@hotmail.com>
2023-02-22 20:37:21 -0300

    feat(users): finishing delete controller

--- src/domains/users/controllers/delete-user.ts
+++ src/domains/users/controllers/delete-user.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express'
 import { Controller } from '../../../shared/interfaces/controller.interface'
+import { UserModel } from '../models/user'
 
 export class DeleteUser implements Controller {
     async handle(req: Request, res: Response) {
-        return res.status(200).send('todo')
+        const params = req.params
+
+        if (!params.uuid) {
+            return res.status(400).json({ message: 'missing param' })
+        }
+
+        const result = new UserModel().deleteOneUser(params.uuid)
+
+        if (!result) {
+            return res.status(500).send('failed to delete')
+        }
+
+        return res.status(200).send()
     }
 }
