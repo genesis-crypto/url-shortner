933f1c4d942c9947c0ae978bdae41076a456c429
Pedro <p-cardozo@hotmail.com>
2023-02-22 20:30:44 -0300

    feat(users): pOST user controllers created

---
+++ src/domains/users/controllers/post-user.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../../shared/interfaces/controller.interface'
+import { UserModel } from '../models/user'
+import { v4 as uuidv4 } from 'uuid'
+
+export class PostUser implements Controller {
+    async handle(req: Request, res: Response) {
+        const body = req.body
+
+        if (!body.name) {
+            return res.status(400).send('missing content')
+        }
+
+        const payload = {
+            id: uuidv4(),
+            name: body.name
+        }
+
+        const result = await new UserModel().createOneUser(payload)
+
+        if (!result) {
+            return res.status(500).send('failed to create user')
+        }
+
+        return res.status(201).send()
+    }
+}
