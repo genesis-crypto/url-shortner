d49e279d60c44f8c1ce913d2e213e1714285aeec
Pedro <p-cardozo@hotmail.com>
2023-02-22 20:29:23 -0300

    feat(users): adding needed routes for users domain

--- src/domains/users/router/index.ts
+++ src/domains/users/router/index.ts
@@ -1,13 +1,22 @@
 import { Router } from "express"
-import { makeGetOneUser, makeGetManyUser } from '../controllers'
+import { makeGetOneUser, makeGetManyUser, makePostUser, makePatchUser, makeDeleteUser } from '../controllers'
 
 const users_path = '/users'
 
 const route = Router()
 const getOneUser = makeGetOneUser()
 const getManyUser = makeGetManyUser()
+const postUser = makePostUser()
+const patchUser = makePatchUser()
+const deleteUser = makeDeleteUser()
 
-route.get('/', getManyUser.handle)
-route.get('/:uuid', getOneUser.handle)
+route.route('/')
+    .get(getManyUser.handle)
+    .post(postUser.handle)
+
+route.route('/:uuid')
+    .get(getOneUser.handle)
+    .patch(patchUser.handle)
+    .delete(deleteUser.handle)
 
 export { route, users_path }
