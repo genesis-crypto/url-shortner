03ddfc165295f081b9b09445397a13fe5877750f
Pedro <p-cardozo@hotmail.com>
2023-02-23 00:15:41 -0300

    feat(source): including links routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import path from 'path'
 import * as dotenv from 'dotenv'
 import { app } from './shared/configs/http'
-import { route, users_path } from './domains/users/router'
+import { route as userRouter, users_path } from './domains/users/router'
+import { route as linkRouter, links_path } from './domains/links/router'
 
 const env_path = path.join(__dirname, '.env')
 dotenv.config({ path: env_path })
 
-app.use(users_path, route)
+app.use(users_path, userRouter)
+app.use(links_path, linkRouter)
 
 app.listen(3000, async () => {
     console.log('server running on 3000')
