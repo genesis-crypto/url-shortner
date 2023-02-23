ddb8ea9d6f941b187a285f6fe90e2980190c73e0
Pedro <p-cardozo@hotmail.com>
2023-02-23 19:40:47 -0300

    feat(swagger): including swagger on /doc path

--- src/shared/configs/http/index.ts
+++ src/shared/configs/http/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import helmet from 'helmet'
+import swaggerUi from 'swagger-ui-express'
+import * as json from '../swagger/swagger.json'
 
 export const app = express()
 
 app.use(helmet())
 app.use(express.json());
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(json))
